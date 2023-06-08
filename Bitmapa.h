5c191fe4fd311991422d6a6788081214957b784c
k4mti <kamil.boloz0407@gmail.com>
2023-06-08 10:44:59 +0200

    Dodanie zawartosci klasy bimapa

--- Bitmapa.h
+++ Bitmapa.h
@@ -1,4 +1,14 @@
 #ifndef BITMAPA_H
 #define BITMAPA_H
 
+class Bitmapa
+{
+public:
+	virtual unsigned length() const = 0;
+	virtual unsigned width() const = 0;
+	virtual bool& operator() (unsigned x, unsigned y) = 0;
+	virtual bool operator() (unsigned x, unsigned y) const = 0;
+	virtual ~Bitmapa() {}
+};
+
 #endif
